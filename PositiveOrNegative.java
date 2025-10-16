63da1dc8902faafc81a7a710918e503f6075123e
Roshita-Guthikonda <roshitaguthikonda28@gmail.com>
2025-10-16 22:27:09 +0530

    Positive number or Negative number

---
+++ PositiveOrNegative.java
@@ -0,0 +1,16 @@
+package com.codegnan.operatorexamples;
+
+import java.util.Scanner;
+
+public class PositiveOrNegative {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number:");
+		int num=sc.nextInt();
+		String result=(num>0)?"Positive number":"Negative number";
+		System.out.println(result);
+		sc.close();
+	}
+
+}
